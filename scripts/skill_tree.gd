4ae1ad9bb83f1bcff0372bf9479f0cc9069e6204
Charamyfavchara <charamyfavchara@gmail.com>
2026-09-19 16:23:27 +0300

    Progressed Skill Tree

--- scripts/skill_tree.gd
+++ scripts/skill_tree.gd
@@ -7,8 +7,17 @@ var jump : int = ability_levels[0]        #top
 var strenght : int = ability_levels[1]     #right
 var dash : int = ability_levels[2]         #left
 
+var able_to_jump : bool = false
+var able_to_strength : bool = false
+var able_to_dash : bool = false
+
+
 
 func _ready() -> void:
+	for i in skill_tree_button_group.get_buttons():
+		i.disabled = true
+	
+	
 	skill_tree_button_group.pressed.connect(button_pressed)
 
 
@@ -41,18 +50,84 @@ func button_pressed(button : Button):
 
 
 func _process(_delta: float) -> void:
+	
+	button_able_disable()
+	
 	$Jump.text = "Jump Level: " + str(ability_levels[0])
 	$Strength.text = "Strengt Level: " + str(ability_levels[1])
 	$Dash.text = "Dash Level: " + str(ability_levels[2])
 
 
-func _on_jump_pressed() -> void:
-	pass # Replace with function body.
+func button_able_disable():
+	if able_to_jump:
+		%Row5Button1.disabled = false
+	if able_to_dash:
+		%Row1Button1.disabled = false
+	if able_to_strength:
+		%Row1Button5.disabled = false
+	if able_to_dash and able_to_jump:
+		%Row5Button1.disabled = false
+		%Row4Button1.disabled = false
+		%Row3Button1.disabled = false
+		%Row2Button1.disabled = false
+		%Row1Button1.disabled = false
+	if able_to_jump and able_to_strength:
+		%Row5Button1.disabled = false
+		%Row4Button2.disabled = false
+		%Row3Button3.disabled = false
+		%Row2Button4.disabled = false
+		%Row1Button5.disabled = false
+	if able_to_strength and able_to_dash:
+		%Row1Button1.disabled = false
+		%Row1Button2.disabled = false
+		%Row1Button3.disabled = false
+		%Row1Button4.disabled = false
+		%Row1Button5.disabled = false
+	if able_to_dash and able_to_jump and able_to_strength:
+		for i in skill_tree_button_group.get_buttons():
+			i.disabled = false
+	
+	
+	if not(able_to_jump):
+		%Row5Button1.disabled = true
+	if not(able_to_dash):
+		%Row1Button1.disabled = true
+	if not(able_to_strength):
+		%Row1Button5.disabled = true
+	if not(able_to_dash and able_to_jump):
+		%Row4Button1.disabled = true
+		%Row3Button1.disabled = true
+		%Row2Button1.disabled = true
+	if not(able_to_jump and able_to_strength):
+		%Row4Button2.disabled = true
+		%Row3Button3.disabled = true
+		%Row2Button4.disabled = true
+	if not(able_to_strength and able_to_dash):
+		%Row1Button2.disabled = true
+		%Row1Button3.disabled = true
+		%Row1Button4.disabled = true
+	if not(able_to_dash and able_to_jump and able_to_strength):
+		%Row3Button2.disabled = true
+		%Row2Button2.disabled = true
+		%Row2Button3.disabled = true
 
 
-func _on_dash_pressed() -> void:
-	pass # Replace with function body.
+func _on_jump_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		able_to_jump = true
+	if toggled_on == false:
+		able_to_jump = false
 
 
-func _on_strength_pressed() -> void:
-	pass # Replace with function body.
+func _on_dash_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		able_to_dash = true
+	if toggled_on == false:
+		able_to_dash = false
+
+
+func _on_strength_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		able_to_strength = true
+	if toggled_on == false:
+		able_to_strength = false
