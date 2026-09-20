5b099a9b2373f3d99798818bbc1482f3123c8543
iboBey333 <ibrahimbeyinli45@gmail.com>
2026-09-20 16:41:40 +0300

    Menu V1

--- scripts/skill_tree.gd
+++ scripts/skill_tree.gd
@@ -2,6 +2,18 @@ extends Control
 
 @export var skill_tree_button_group : ButtonGroup
 
+@onready var links_horizontal_1: Line2D = %LinksHorizontal1
+@onready var links_horizontal_2 : Line2D = %LinksHorizontal2
+@onready var links_horizontal_3 : Line2D = %LinksHorizontal3
+@onready var links_horizontal_4 : Line2D = %LinksHorizontal4
+@onready var links_vertical_1: Line2D = %LinksVertical1
+@onready var links_vertical_2 : Line2D = %LinksVertical2
+@onready var links_vertical_3 : Line2D = %LinksVertical3
+@onready var links_vertical_4 : Line2D = %LinksVertical4
+@onready var links_horizontal : Array = [links_horizontal_1, links_horizontal_2, links_horizontal_3, links_horizontal_4]
+
+
+
 var ability_levels : Array = [1,1,1] # [jump,strenght,dash] [green,blue,red]
 var jump : int = ability_levels[0]        #top
 var strenght : int = ability_levels[1]     #right
@@ -11,14 +23,19 @@ var able_to_jump : bool = false
 var able_to_strength : bool = false
 var able_to_dash : bool = false
 
+#color variables
 var module_array : Array = []
 var color_step : float = 255.0 / 4.0
+#link variables
+var link_correction_offset : Vector2 = Vector2(-8,181.875)
 
 func _ready() -> void:
 	for i in skill_tree_button_group.get_buttons():
 		i.disabled = true
 	
 	skill_tree_button_group.pressed.connect(button_pressed)
+	
+	skill_linking()
 
 
 func refresh_toggle(_button : Button):
@@ -74,9 +91,9 @@ func _process(_delta: float) -> void:
 	
 	button_able_disable()
 	
-	$JumpLabel.text = "Jump Level: " + str(ability_levels[0])
-	$DashLabel.text = "Strength Level: " + str(ability_levels[1])
-	$StrengthLabel.text = "Dash Level: " + str(ability_levels[2])
+	%JumpLabel.text = "Jump Level: " + str(ability_levels[0])
+	%DashLabel.text = "Strength Level: " + str(ability_levels[1])
+	%StrengthLabel.text = "Dash Level: " + str(ability_levels[2])
 
 
 func button_able_disable():
@@ -146,8 +163,32 @@ func _on_dash_button_toggled(toggled_on: bool) -> void:
 	if toggled_on == false:
 		able_to_dash = false
 
+
 func _on_strength_button_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
 		able_to_strength = true
 	if toggled_on == false:
 		able_to_strength = false
+
+
+#Skill Links
+
+func skill_linking():
+
+	await get_tree().process_frame
+
+	get_tree().call_group("SkillLinks","clear_points")
+	
+	var h_start_array : Array = [%Row1Button1.global_position / 8,
+	 %Row2Button1.global_position / 8,
+	 %Row3Button1.global_position / 8,
+	 %Row4Button1.global_position / 8]
+	
+	var h_step = Vector2(+16,0)
+	var h_points : int = 5
+	
+	for i in range(links_horizontal.size()):
+		for point in range(h_points):
+			links_horizontal[i].add_point(h_start_array[i] + link_correction_offset + point * h_step)
+		h_points -= 1
+	
