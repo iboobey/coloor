676113d943cd337b87a7e5c91a8422e6ce58793f
Charamyfavchara <charamyfavchara@gmail.com>
2026-09-20 22:00:17 +0300

    shut up ibo

--- scripts/blobby.gd
+++ scripts/blobby.gd
@@ -23,9 +23,21 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, walk_speed)
 
 	move_and_slide()
-	
-	
-	if Input.is_action_just_pressed("down") and is_on_floor():
-		velocity.x
-	
-	
+
+const BULLET_SCENE=preload("res://scenes/bullet.tscn")
+@export var shoot_cooldown: float = 0.5
+var can_shoot: bool = true
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("down") and can_shoot:
+		shoot()
+
+func shoot() -> void:
+	can_shoot = false
+	var bullet = BULLET_SCENE.instantiate()
+	var dir = -1 if $Sprite2D.flip_h else 1
+	bullet.direction = dir
+	bullet.global_position = global_position
+	get_tree().current_scene.add_child(bullet)
+	await get_tree().create_timer(shoot_cooldown).timeout
+	can_shoot = true
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-@export var speed: float = 10
+@export var speed: float = 50
 var direction: float = 1.0
 func _physics_process(delta: float) -> void:
 	# Move the bullet horizontally based on direction fired
 	position.x += direction*speed*delta
 # Delete the bullet when it leaves the screen or it hits something
 func _on_body_entered(body:Node2D) -> void:
-	if body.is_in_group("enemies"):
+	if body.is_in_group("glass"):
 		body.queue_free()
 	queue_free()
