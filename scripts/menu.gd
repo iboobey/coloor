3f423cb188144b0538de585f82102199e474ca80
iboBey333 <ibrahimbeyinli45@gmail.com>
2026-09-24 18:54:34 +0300

    Color Wheels

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,6 +7,10 @@ var green = Global.player_green
 var blue = Global.player_blue
 var player_module : Color = Color(red,green,blue)
 
+var red_frame : int = 0
+var green_frame : int = 0
+var blue_frame : int = 0
+
 @onready var skill_tree: Control = $MenuTab/SkillTree
 @onready var blobby_accesories: Control = $MenuTab/BlobbyAccesories
 
@@ -18,10 +22,7 @@ func _process(_delta: float) -> void:
 	player_module = Color.from_rgba8(red,green,blue)
 	blobby.modulate = player_module
 	
-	
-	
-	
-	
+	color_wheel()
 
 
 func hide_instances():
@@ -49,3 +50,44 @@ func _on_skill_tree_pressed() -> void:
 
 func _on_menu_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func color_wheel():
+	red = Global.player_red
+	var red_index : int = 3 * red / 63.75
+	red_frame = red_index
+	green = Global.player_green
+	var green_index : int = 3 * green / 63.75
+	green_frame = green_index
+	blue = Global.player_blue
+	var blue_index : int = 3 * blue / 63.75
+	blue_frame = blue_index
+	
+	if red_frame < %RedWheel.frame:
+		%RedWheel.speed_scale = -1.0
+		%RedWheel.play("Red")
+	elif red_frame > %RedWheel.frame:
+		%RedWheel.speed_scale = 1.0
+		%RedWheel.play("Red")
+	else:
+		%RedWheel.pause()
+	
+	
+	if green_frame < %GreenWheel.frame:
+		%GreenWheel.speed_scale = -1.0
+		%GreenWheel.play("Green")
+	elif green_frame > %GreenWheel.frame:
+		%GreenWheel.speed_scale = 1.0
+		%GreenWheel.play("Green")
+	else:
+		%GreenWheel.pause()
+	
+	
+	if blue_frame < %BlueWheel.frame:
+		%BlueWheel.speed_scale = -1.0
+		%BlueWheel.play("Blue")
+	elif blue_frame > %BlueWheel.frame:
+		%BlueWheel.speed_scale = 1.0
+		%BlueWheel.play("Blue")
+	else:
+		%BlueWheel.pause()
